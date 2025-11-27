413a0f23f2da0cd4bc421e2e89dd5b3949b799b1
Leon <gomdole32@naver.com>
2025-11-27 18:59:55 +0900

    New: 유저 및 그룹 라우트 추가

--- src/routes/badge-routes.js
+++ src/routes/badge-routes.js
@@ -1,29 +1,33 @@
 // TODO) Badge-Routes: 배지 관련 URL 매핑
+// &) Config Import
 import express from 'express';
+
+// &) Core Import
 import asyncHandler from '../core/error/async-handler.js';
+
+// &) Middleware Import
 import { requireAuth } from '../middlewares/auth.js';
+
+// &) Controller Import
 import { badgeController } from '../controllers/badge-controller.js';
 
+// ?) Express 라우터 생성
 const router = express.Router();
 
-// 배지 정의
+// ?) 배지 정의
 router.get('/', asyncHandler(badgeController.listBadges));
 router.post('/', requireAuth, asyncHandler(badgeController.createBadge));
 
-// 유저 배지
+// ?) 유저 배지
 router.get('/users/:userId', requireAuth, asyncHandler(badgeController.listUserBadges));
-router.post(
-  '/:badgeId/users/:userId',
-  requireAuth,
-  asyncHandler(badgeController.grantUserBadge),
-);
+router.post('/:badgeId/users/:userId', requireAuth, asyncHandler(badgeController.grantUserBadge));
 router.delete(
   '/:badgeId/users/:userId',
   requireAuth,
   asyncHandler(badgeController.revokeUserBadge),
 );
 
-// 그룹 배지
+// ?) 그룹 배지
 router.get('/groups/:groupId', requireAuth, asyncHandler(badgeController.listGroupBadges));
 router.post(
   '/:badgeId/groups/:groupId',
