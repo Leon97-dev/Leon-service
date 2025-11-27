65ea81542b2cb8e12995be976f97b44a76cd9be8
Leon <gomdole32@naver.com>
2025-11-27 15:17:38 +0900

    NEW: SEVEN 초기 설정 세팅

---
+++ public/project-seven-fe-main/next.config.ts
@@ -0,0 +1,34 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000', // 백엔드 포트
+      },
+      {
+        protocol: 'https',
+        hostname: 'seven-api-f5mj.onrender.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sprint-be-project.s3.ap-northeast-2.amazonaws.com',
+      },
+    ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/groups/:groupId((?!/new).)*',
+        destination: '/groups/:groupId*/records',
+        permanent: false,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
